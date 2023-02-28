df3568f353555d41dca963054f943a83488aebc9
Marina Egorova <cammelen@yandex.ru>
2023-02-28 15:51:22 +0300

    Diplom_1 commit

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,11 +1,10 @@
-package praktikum;
-
 import org.junit.Assert;
 import org.junit.Test;
+import praktikum.Bun;
 
 public class BunTest {
 
-    private Bun bun = new Bun("black bun", 100);
+    private final Bun bun = new Bun("black bun", 100);
 
     @Test
     public void getNameBun() {
